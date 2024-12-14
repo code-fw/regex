a944a74a2ef3b5669f96fa308ae944facd8421f6
code-fw <estrada.flsw@gmail.com>
2024-12-13 20:54:03 -0600

    create basic app function

--- js/script.js
+++ js/script.js
@@ -8,9 +8,14 @@ const btnLang = $('#btnLang');
 const menuLang = $('#menuLang');
 const btnLangEn = $('#btnLangEn');
 const btnLangEs = $('#btnLangEs');
-const toTraslate = ['title', 'description']
-
+const toTraslate = ['title', 'description', 'h1Index', 'h2Description', 'labelConsole', 'labelTest'];
+const consoleRegex = $('#consola');
+const testRegex = $('#test');
+const contentTest = $('#contentTest');
+const iconTest = $('#iconTest');
 const iconCheck = `<svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.8"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>`
+const iconX = `<svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.8"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>`
+const iconAlert = `<svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.8"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-alert-square-rounded"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" /><path d="M12 8v4" /><path d="M12 16h.01" /></svg>`
 
 // Theme
 if (currentTheme === 'dark') {
@@ -79,4 +84,42 @@ async function loadTranslations(lang) {
 document.addEventListener('DOMContentLoaded', () => {
     const savedLang = localStorage.getItem('languague') || 'en'
     setLanguague(savedLang)
+})
+
+// Regular expressions
+function validateRegex() {
+    try {
+        const regexContent = consoleRegex.textContent.trim()
+        const regex = new RegExp(regexContent)
+
+        if (regex.test(testRegex.value)) {
+            contentTest.classList.remove('border-red-500', 'border-amber-500')
+            contentTest.classList.add('border-green-500')
+            iconTest.classList.remove('text-red-500', 'text-amber-500')
+            iconTest.classList.add('text-green-500')
+            iconTest.innerHTML = iconCheck
+        } else {
+            contentTest.classList.remove('border-green-500', 'border-amber-500')
+            contentTest.classList.add('border-red-500')
+            iconTest.classList.remove('text-green-500', 'text-amber-500')
+            iconTest.classList.add('text-red-500')
+            iconTest.innerHTML = iconX
+        }
+    } catch(error) {
+        contentTest.classList.remove('border-green-500')
+        contentTest.classList.add('border-amber-500')
+        iconTest.classList.remove('text-green-500')
+        iconTest.classList.add('text-amber-500')
+        testRegex.setCustomValidity = 'Invalid regular expression'
+        iconTest.innerHTML = iconAlert
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    validateRegex()
+    testRegex.addEventListener('click', validateRegex)
+    testRegex.addEventListener('input', validateRegex)
+    testRegex.addEventListener('blur', validateRegex)
+    consoleRegex.addEventListener('input', validateRegex)
+    consoleRegex.addEventListener('blur', validateRegex)
 })
