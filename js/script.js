f791cd7306890d26442c5c1cf09eec14e390de19
code-fw <estrada.flsw@gmail.com>
2024-12-18 19:23:56 -0600

    edit bg on collection cards

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ async function loadCollection() {
                         <h5 id="${key}Title" class="font-bold"></h5>
                         <p class="mb-0"><small id="${key}Description" class="text-xs"></small></p>
                     </div>
-                    <div class="p-3 flex items-center justify-between gap-3 rounded-b-lg bg-white/90 dark:bg-stone-950/60">
+                    <div class="p-3 flex items-center justify-between gap-3 rounded-b-lg bg-white/60 dark:bg-stone-950/60">
                         <span class="font-mono text-zinc-950 text-nowrap overflow-hidden dark:text-zinc-50">/${collection[key]}/</span>
                         <button data-key="${key}" class="copy-btn text-black dark:text-white" title="Copy"><svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.8"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-copy"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" /><path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" /></svg></button>
                     </div>
